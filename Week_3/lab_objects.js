309f9d70a3d3ec2095ffdea60c3347f44ab2fe94
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-05 17:55:21 -0800

    Update lab_objects.js
    completed lab using destructuring and clearing object

--- Week_3/lab_objects.js
+++ Week_3/lab_objects.js
@@ -30,6 +30,7 @@ darthVader.childNames = ['Luke', 'Leia'];
 // (c)
 console.log("Darth Vader\'s amount of children: ", darthVader.children);
 console.log("Darth\'s first child: ", darthVader.childNames[0]);
+
 // Instruction 6 - iterate over the object
 // (a)
 for (var key in darthVader) {
@@ -39,16 +40,15 @@ for (var key in darthVader) {
 }
 
 // Instruction 7 - manipulate the object
-// (a), (b), (c), (d)
-
-/*6. For this step, perform the following:
-    a) Iterate over darthVader using a for...in loop that uses both the key and the value of each pair;
-    b) Then, console-logging out each pair.
-
-7. Manipulate the object by:
-    a) Changing the value of allegiance to The light side and console-log darthVader;
-    b) Deleting the key/value pair children of darthVader and console-log darthVader. Hint: use the code below:
-        delete darthVader.children;
-    c) De-structuring the object, setting a variable for each of the keys in the object to the corresponding value in the object:
-        let {allegiance, weapon, sith, childNames} = darthVader;
-    d) Lastly, clearing the darthVader object and console log it.*/
+// (a)
+darthVader.allegiance = "The light side";
+console.log(darthVader);
+// (b)
+delete darthVader.children;
+// (c)
+let {allegiance, weapon, sith, childNames} = darthVader;
+// (d)
+for (const key in darthVader) {
+    delete darthVader[key];
+  }
+console.log(darthVader);
