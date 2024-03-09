c5011c006f8115310fc7b16d445dc874cb682687
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-09 12:17:31 -0800

    Copied first HW Submission to folder to keep code safe

---
+++ Week_3/HW_2_Submission/App1.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const react = ['NPM', 'Node.js', 'JXS', 'Babel', 'Transpiler', 'Props', 'Single Page Application'];
+
+  function renderList(react) {
+    
+    let map1 = react.map((item, index) => (
+      <li key={index}>{item}</li>
+    ));
+
+    let returnElement = (
+      <ol>
+        {map1}
+      </ol>
+    )
+    
+    return returnElement;
+  }
+  
+  
+  return (
+     // YOUR JAVASCRIPT MAP() AND JSX CODE HERE
+    //Note to self: the <> and </> are very important to compiling and rendering the code in react
+    <>
+    {/*No Function used*/}
+      <ol>
+        {react.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ol>
+    
+    {/*Function used*/}
+    <img src={logo} className="App-logo" alt="logo" />
+    {renderList(react)}
+    </>
+    
+  );
+}
+export default App;
