393b21a3f01c16402d173963662aecdb125f3c8c
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-16 15:39:33 -0700

    Copying updated files to course folder

--- Week_4/HW_3_RenderingCardsUsingProps/App.js
+++ Week_4/HW_3_RenderingCardsUsingProps/App.js
@@ -3,41 +3,16 @@ import data from "./data"
 import Card from "./Card"
 
 function App() {
- 
-    // YOUR CODE HERE
-    // {
-    //     id: 1,
-    //     title: "Nutrition info about Apple",
-    //     description: "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus.",
-    //     price: 1.00,
-    //     coverImg: "apple.jpg",
-    //     stats: {
-    //         kingdom: "Plantae",
-    //         family: "Rosaceae",
-    //         genus: "Malus"
-    //     },
-    //     location: "Japan"
-    // }
-    
+    //Mapping items from data, sending it to Card, and receiving the rendered version
+    const items = data.map((item, index) => (
+        //similar to <List /> from video
+        <Card key={index} item={item} />
+    ));
 
-        return(
-        <div>
-            <p>
-            {
-                data.map(item => 
-                    <>
-                        <div><img src={item.coverImg}/></div> 
-                        <div>{item.title}</div>
-                        <div><b>Average Cost ${item.price}</b> / LB</div>
-                        <div>Kingdom: {item.stats.kingdom}</div>
-                        <div>Family: {item.stats.family}</div>
-                        <div>Genus: {item.stats.genus}</div>
-                        <div>Origin: {item.location}</div>
-                    </>
-                )
-            }
-            </p>
-        </div>
+    return(
+        <>
+            {items}
+        </>
     );
 }
 
