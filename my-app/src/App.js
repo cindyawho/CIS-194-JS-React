9a0bdc3097b2c939abbee2d2cd45b1324e700068
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-16 15:15:33 -0700

    Cleaning up code- deleting comments and console logs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,47 +16,15 @@ import Card from "./Card"
 function App() {
     //const appleImage = new fileName("./", "1.coverImg");
 
-    const[item1, item2, item3] = data;
-    console.log("item1 in data: ", item1);
-    console.log("item2 in data: ", item2);
-    console.log("item3 in data: ", item3);
-    console.log("END OF CONSOLE LOG.");
-
     const items = data.map((item, index) => (
         <Card key={index} item={item} />
     ));
 
     return(
-        // {const{item1, item2, item3} = data;
-        // {console.log("This is item1 in data: ", item1);}
-        // console.log("END OF CONSOLE LOG.");}
         <>
-        <p>Hello</p>
-        {items}
-        {/* <Card
-            //item1 = {item1}
-            {data.map(item => 
-                item1 = item);}
-        /> */}
+            <p>Hello</p>
+            {items}
         </>
-        // <div>
-        //     <p>
-        //     {
-        //         data.map(item => 
-        //             <>
-        //                 {console.log(item)};
-        //                 <div><img src={item.coverImg}/></div> {/*?use sentence function to combine "./" with coverImg?*/}
-        //                 <div>{item.title}</div>
-        //                 <div><b>Average Cost ${item.price}</b> / LB</div>
-        //                 <div>Kingdom: {item.stats.kingdom}</div>
-        //                 <div>Family: {item.stats.family}</div>
-        //                 <div>Genus: {item.stats.genus}</div>
-        //                 <div>Origin: {item.location}</div>
-        //             </>
-        //         )
-        //     }
-        //     </p>
-        // </div>
     );
 }
 // const imageFile = props => {
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,32 +1,24 @@
 import React from "react"
 
 export default function Card(props) {
-  console.log("This is the props in the card: ", props)
   const{id, title, description, price, coverImg, stats, location} = props.item;
-  console.log("ID Saved: ", id);
-  console.log("Title saved: ", title);
-  console.log("Description saved: ", description);
-  console.log("Price saved: ", price);
+  
   console.log("CoverImg saved: ", coverImg);
-  console.log("Stats saved: ", stats);
-  console.log("Location saved: ", location);
   
   return(
     <div>
       <p>
         {
-            // data.map(item => 
-                <>
-                    {/* {console.log(item)}; */}
-                    {/* <div><img src={coverImg}/></div> ?use sentence function to combine "./" with coverImg? */}
-                    <div>{title}</div>
-                    <div><b>Average Cost ${price}</b> / LB</div>
-                    <div>Kingdom: {stats.kingdom}</div>
-                    <div>Family: {stats.family}</div>
-                    <div>Genus: {stats.genus}</div>
-                    <div>Origin: {location}</div>
-                </>
-            // )
+          <>
+            {/* {console.log(item)}; */}
+            {/* <div><img src={coverImg}/></div> ?use sentence function to combine "./" with coverImg? */}
+            <div>{title}</div>
+            <div><b>Average Cost ${price}</b> / LB</div>
+            <div>Kingdom: {stats.kingdom}</div>
+            <div>Family: {stats.family}</div>
+            <div>Genus: {stats.genus}</div>
+            <div>Origin: {location}</div>
+          </>
         }
       </p>
     </div>
