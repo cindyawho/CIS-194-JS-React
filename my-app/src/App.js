0a9f769f0cefccbc18983d79bc3fbbd55bf2cec5
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-16 15:26:48 -0700

    Cleaning Up code, deleting comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,36 +2,18 @@ import './App.css';
 import data from "./data"
 import Card from "./Card"
 
-// class fileName {
-//     constructor(fileStart, fileEnd){
-//         this.fileStart = fileStart;
-//         this.fileEnd = fileEnd;
-//     }
-
-//     getSentence(){
-//         return this.fileStart + this.fileEnd;
-//     }
-// }
-
 function App() {
-    //const appleImage = new fileName("./", "1.coverImg");
-
+    //Mapping items from data, sending it to Card, and receiving the rendered version
     const items = data.map((item, index) => (
+        //similar to <List /> from video
         <Card key={index} item={item} />
     ));
 
     return(
         <>
-            <p>Hello</p>
             {items}
         </>
     );
 }
-// const imageFile = props => {
-//     if(props=="1"){
-//         return("./item.coverImg");
-//     }
-//     return ("a");
-// }
 
 export default App;
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -3,8 +3,9 @@ import React from "react"
 export default function Card(props) {
   const{id, title, description, price, coverImg, stats, location} = props.item;
   
-  console.log("CoverImg saved: ", coverImg);
+  //console.log("CoverImg saved: ", coverImg);
   const fileName = "images/" + coverImg;
+  const altDesc = "Cover Image of " + title;
   
   return(
     <div>
@@ -12,7 +13,7 @@ export default function Card(props) {
         {
           <>
             {/* {console.log(item)}; */}
-            <div><img src={fileName} alt="Cover Image of fruit"/></div> {/*?use sentence function to combine "./" with coverImg? */}
+            <div><img src={fileName} alt={altDesc}/></div> {/*?use sentence function to combine "./" with coverImg? */}
             <div>{title}</div>
             <div><b>Average Cost ${price}</b> / LB</div>
             <div>Kingdom: {stats.kingdom}</div>
