3f78589cf43a17b0b17e6832b8046cc61f3fd8e4
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-23 10:59:48 -0700

    React site now renders- finally!!! needed to export default App at the end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,41 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 const { useState } = React
 
+const list = [
+    {
+      title: "React Homepage",
+      url: "https://reactjs.org/",
+    },
+    {
+      title: "Getting Started",
+      url: "https://reactjs.org/docs/getting-started.html",
+    },
+    {
+      title: "Hello World",
+      url: "https://reactjs.org/docs/hello-world.html",
+    },
+    {
+      title: "Introducing JSX",
+      url: "https://reactjs.org/docs/introducing-jsx.html",
+    },
+    {
+      title: "Rendering Elements",
+      url: "https://reactjs.org/docs/rendering-elements.html",
+    }
+  ];  
 
 const App = () => {
-    
+  
   const [input, setValue] = useState("");
   const [name, setName] = useState('Joe');
   
-  handleInput = (event) => {
+  const handleInput = (event) => {
     console.log(event.target.value);
     setValue(event.target.value);
   }
   
-  updateName = (event) => {
+  const updateName = (event) => {
     event.preventDefault();
     setName(input);
     setValue("");
@@ -49,18 +72,23 @@ const Search = props => {
         <div>
             <h1>Search</h1>
             <label htmlFor='Check'>Check: </label>
-            <input id="check" type="text" onChange={handleEvent}/>
-            <p>Checking for <b>{checkTerm}</b></p>
+            {/* <input id="check" type="text" onChange={handleInput}/> */}
         </div>
     );
 }
 
 const List = props => {
-    
+    const items = list.map((item, index) => (
+        //similar to <List /> from video
+        {item}
+    ));
     return (
-        <div>List</div>
+        <div>List
+           {items} 
+        </div>
     );
 }
 
 ReactDOM.render(<App />,
 document.getElementById("root"))
+export default App;
