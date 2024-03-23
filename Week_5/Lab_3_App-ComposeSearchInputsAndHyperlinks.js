bc414db60428c255f7404d761ec2893d3019f11a
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-23 11:13:00 -0700

    Saving Lab 3 File, setting up lab 4

--- Week_5/Lab_3_App-ComposeSearchInputsAndHyperlinks.js
+++ Week_5/Lab_3_App-ComposeSearchInputsAndHyperlinks.js
@@ -1,58 +1,101 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom';
+const { useState } = React
+
+const toc = [
+    {
+      title: "React Homepage",
+      url: "https://reactjs.org/",
+    },
+    {
+      title: "Getting Started",
+      url: "https://reactjs.org/docs/getting-started.html",
+    },
+    {
+      title: "Hello World",
+      url: "https://reactjs.org/docs/hello-world.html",
+    },
+    {
+      title: "Introducing JSX",
+      url: "https://reactjs.org/docs/introducing-jsx.html",
+    },
+    {
+      title: "Rendering Elements",
+      url: "https://reactjs.org/docs/rendering-elements.html",
+    }
+  ];  
 
 const App = () => {
-
-  const toc = [
-    {
-      title: 'React Homepage',
-      url: 'https://reactjs.org/',
-    },
-    {
-      title: 'Getting Started',
-      url: 'https://reactjs.org/docs/getting-started.html',
-    },
-    {
-      title: 'Hello World',
-      url: 'https://reactjs.org/docs/hello-world.html',
-    },
-    {
-      title: 'Introducing JSX',
-      url: 'https://reactjs.org/docs/introducing-jsx.html',
-    },
-    {
-      title: 'Rendering Elements',
-      url: 'https://reactjs.org/docs/rendering-elements.html',
-    }
-  ];
-
+  
+  const [input, setValue] = useState("");
+  const [name, setName] = useState('Joe');
+  
+  const handleInput = (event) => {
+    console.log(event.target.value);
+    setValue(event.target.value);
+  }
+  
+  const updateName = (event) => {
+    event.preventDefault();
+    setName(input);
+    setValue("");
+  }
+  
   return (
-    <div>
-      <h1>React: A JavaScript library</h1>
-
-      // Creating a search component
-      YOUR_CODE_HERE
-
-      <hr />
-
-      // Creating a List component
-      <List results = {toc}/>
+    <div className="box">
+      <h1>
+        Hello, <span>{name}!</span>   
+      </h1>
+      
+      <form className="form">
+        
+        <div class="field">
+          <label for="name-1">Update Name</label>
+          <div class="control">
+            <input type="text" value={input} name="name-1" onChange={handleInput} class="input"/>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button onClick={updateName} class="button is-dark">Save</button>
+          </div>
+        </div>
+      </form>
+      <div>
+        <Search />
+      </div>
+      <div>
+        <List />
+      </div>
     </div>
-  );
-};
+  )
+}
 
-const Search = props => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input type="text" />
-  </div>
-);
+const Search = () => {
 
-const List = props =>
-  props.results.map(item => (
-    <div>
-      YOUR_CODE_HERE
-    </div>
-  ));
+    return (
+        <div>
+            <h1>Search  
+            <input id="check" type="text" />
+            </h1>
+        </div>
+    );
+}
 
+const List = () => {
+    const items = toc.map((item, index) => (
+        <div>
+            <p><a href={item.url}>{item.title}</a></p>
+        </div>
+    ));
+    return (
+        <div>
+            <h1>List</h1>
+            {items} 
+        </div>
+    );
+}
 
+ReactDOM.render(<App />,
+document.getElementById("root"))
 export default App;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,101 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-const { useState } = React
+import React from 'react';
 
-const list = [
-    {
-      title: "React Homepage",
-      url: "https://reactjs.org/",
-    },
-    {
-      title: "Getting Started",
-      url: "https://reactjs.org/docs/getting-started.html",
-    },
-    {
-      title: "Hello World",
-      url: "https://reactjs.org/docs/hello-world.html",
-    },
-    {
-      title: "Introducing JSX",
-      url: "https://reactjs.org/docs/introducing-jsx.html",
-    },
-    {
-      title: "Rendering Elements",
-      url: "https://reactjs.org/docs/rendering-elements.html",
-    }
-  ];  
+// This is your callback from Search input. You will
+// take the event and print its value to the console 
+// log
+const handleSearch = {
+    
+}
 
 const App = () => {
-  
-  const [input, setValue] = useState("");
-  const [name, setName] = useState('Joe');
-  
-  const handleInput = (event) => {
-    console.log(event.target.value);
-    setValue(event.target.value);
-  }
-  
-  const updateName = (event) => {
-    event.preventDefault();
-    setName(input);
-    setValue("");
-  }
-  
   return (
-    <div className="box">
-      <h1>
-        Hello, <span>{name}!</span>   
-      </h1>
-      
-      <form className="form">
-        
-        <div class="field">
-          <label for="name-1">Update Name</label>
-          <div class="control">
-            <input type="text" value={input} name="name-1" onChange={handleInput} class="input"/>
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <button onClick={updateName} class="button is-dark">Save</button>
-          </div>
-        </div>
-      </form>
-      <div>
-        <Search />
-      </div>
-      <div>
-        <List />
-      </div>
+    <div>
+      <h1>React: A JavaScript library</h1>
+
+      // Creating a search component with a props
+      <Search onSearch={handleSearch} />
+
     </div>
-  )
-}
+  );
+};
 
-const Search = () => {
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch}/>
+  </div>
+);
 
-    return (
-        <div>
-            <h1>Search  
-            <input id="check" type="text" />
-            </h1>
-        </div>
-    );
-}
-
-const List = () => {
-    const items = list.map((item, index) => (
-        <div>
-            <p><a href={item.url}>{item.title}</a></p>
-        </div>
-    ));
-    return (
-        <div>
-            <h1>List</h1>
-            {items} 
-        </div>
-    );
-}
-
-ReactDOM.render(<App />,
-document.getElementById("root"))
 export default App;
