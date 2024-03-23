0135ad891af1a36d6234b858e5fdadccbb46f5ba
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-23 10:47:03 -0700

    working on Lab 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import './index.css';
+import React from 'react'
+const { useState } = React
+
 
 const App = () => {
+    
+  const [input, setValue] = useState("");
+  const [name, setName] = useState('Joe');
+  
+  handleInput = (event) => {
+    console.log(event.target.value);
+    setValue(event.target.value);
+  }
+  
+  updateName = (event) => {
+    event.preventDefault();
+    setName(input);
+    setValue("");
+  }
+  
   return (
-    <div>
-      <Title  name="React"/>
-      <SubTitle />
+    <div className="box">
+      <h1>
+        Hello, <span>{name}!</span>   
+      </h1>
+      
+      <form className="form">
+        
+        <div class="field">
+          <label for="name-1">Update Name</label>
+          <div class="control">
+            <input type="text" value={input} name="name-1" onChange={handleInput} class="input"/>
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button onClick={updateName} class="button is-dark">Save</button>
+          </div>
+        </div>
+      </form>
+      
     </div>
-  );
-};
-
-const Title = props => (
-  <h1>{props.name}</h1>
-);
-
-function SubTitle() {
-  return <p>A JavaScript library for building user interfaces.</p>;
+  )
 }
 
-export default App;
+const Search = props => {
+
+    return (
+        <div>
+            <h1>Search</h1>
+            <label htmlFor='Check'>Check: </label>
+            <input id="check" type="text" onChange={handleEvent}/>
+            <p>Checking for <b>{checkTerm}</b></p>
+        </div>
+    );
+}
+
+const List = props => {
+    
+    return (
+        <div>List</div>
+    );
+}
+
+ReactDOM.render(<App />,
+document.getElementById("root"))
---
+++ my-app/src/tocData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      title: "React Homepage",
+      url: "https://reactjs.org/",
+    },
+    {
+      title: "Getting Started",
+      url: "https://reactjs.org/docs/getting-started.html",
+    },
+    {
+      title: "Hello World",
+      url: "https://reactjs.org/docs/hello-world.html",
+    },
+    {
+      title: "Introducing JSX",
+      url: "https://reactjs.org/docs/introducing-jsx.html",
+    },
+    {
+      title: "Rendering Elements",
+      url: "https://reactjs.org/docs/rendering-elements.html",
+    }
+  ];
