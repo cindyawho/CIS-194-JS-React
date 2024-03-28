197885257d66b0547f5dc4eb8edf93a41f053f03
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-03-28 13:26:01 -0700

    Create 2App-ControlledAndUncontrolledComponents.js

---
+++ Week_6/2App-ControlledAndUncontrolledComponents.js
@@ -0,0 +1,66 @@
+import './App.css';
+import React from 'react';
+
+class Sentence {
+    constructor(word1, word2){
+        this.word1 = word1;
+        this.word2 = word2;
+    }
+
+    getSentence(){
+        return this.word1 + " " + this.word2;
+    }
+}
+
+const App = () => {
+    const handleInput = evt => {
+        // Is only triggered when the Input is called
+        setCheckTerm(evt.target.value);
+        console.log(evt.target.value);
+    };
+
+    const helloWorld = new Sentence("Hello", "World!");
+    const reactRocks = new Sentence("React", "rocks!");
+
+    const[checkTerm, setCheckTerm] = React.useState("nothing");
+    // State gets initialized
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    {/* Child 1: List */}
+                    <List first={helloWorld} second={reactRocks} />
+                </p>
+                <p>
+                    {/* Child 2: Input */}
+                    <Input onInput = {handleInput} userInput = {checkTerm} />
+                </p>
+            </header>
+        </div>
+    );
+};
+
+const Input = props => {
+    // Cleaner version, will go to onInput
+    return(
+        <div>
+            <label htmlFor='Check'>Check: </label>
+            <input id="check" type="text" onChange={props.onInput}/>
+            <p>Checking for <b>{props.userInput}</b></p>
+        </div>
+    )
+}
+
+const List = props => {
+    
+    return (
+        <div>
+            {props.first.getSentence()}
+            <hr/>
+            {props.second.getSentence()}
+        </div>
+    );
+}
+
+export default App;
