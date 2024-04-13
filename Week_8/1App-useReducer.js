710bc13ca79751ec0ba4c4100399a457af790a3d
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-04-13 12:56:29 -0700

    Updated useReducer using Fetch API with many more links

--- Week_8/1App-useReducer.js
+++ Week_8/1App-useReducer.js
@@ -1,26 +1,8 @@
 import './App.css';
 import React from 'react'
 
-const initialBookmarks = [
-    {
-        title: "React home",
-        url: "https://reactjs.org",
-    },
-    {
-        title: "Google Link",
-        url: "https://google.com",
-    },
-];
-
-const getAsynchBookmarks = () =>
-//     new Promise(resolve =>
-//         setTimeout(
-//             () => resolve({data: {x: initialBookmarks}}),
-//             3000
-//         )
-// );
-// creating artificial errors, the issue is that we get both an error and loading message at the same time which is a bad user experience
-new Promise((resolve, reject) => setTimeout(reject, 3000));
+// using an API for our bookmarks now
+const bookmarksEndpoint = "https://hn.algolia.com/api/v1/search?query=";
 
 const bookmarksReducer = (state, action) => {
     switch(action.type) {
@@ -51,8 +33,6 @@ const bookmarksReducer = (state, action) => {
 
 function App() {
 
-    // const [bookmarks, setBookmarks] = React.useState([""]); //replacing with useReducer
-    //const[state, dispatch] where state is the new state, dispatch is the updater that takes in type and action/payload
     const [bookmarks, dispatchBookmarks] = React.useReducer(
         bookmarksReducer,
         {
@@ -61,30 +41,30 @@ function App() {
             isError: false 
         }
     );
-    //Replacing the two lines below with new code in useReducer
-    // const[isLoading, setIsLoading] = React.useState(false);
-    // const [isError, setIsError] = React.useState(false);
 
     React.useEffect(() => {
-        // setIsLoading(true);
         dispatchBookmarks(
             {type: "BOOKMARKS_LOADING_INIT"}
         )
-        getAsynchBookmarks().then( result => {
-            // setBookmarks(result.data.x); //replaced by dispatch and payload line below
-            dispatchBookmarks( {
-                type: "BOOKMARKS_LOADING_SUCCESS", 
-                payload: result.data.x,
-            });
-            // setIsLoading(false);
-        }).catch(
-            () => dispatchBookmarks({type: "BOOKMARKS_LOADING_FAILURE"})
-        );
+        //concatenating url with react for the query
+        fetch(`${bookmarksEndpoint}react`) 
+            .then(response => response.json())
+            .then(result => {
+                dispatchBookmarks( {
+                    type: "BOOKMARKS_LOADING_SUCCESS", 
+                    payload: result.hits
+                })
+            }).catch(
+                () => dispatchBookmarks(
+                    {
+                        type: "BOOKMARKS_LOADING_FAILURE"
+                    })
+            )
     }, []);
 
     return(
         <div>
-            <h1>Please wait 3 seconds</h1>
+            <h1>Links fetched from API</h1>
             <p>
                 {bookmarks.isError && <p>Error just happened ... </p>}
                 {/* (condition) ? exe1 : exe2 */}
