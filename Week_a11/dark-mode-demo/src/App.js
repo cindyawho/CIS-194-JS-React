499d691c4ff07dc41b74597e9ef9988d0d293c2c
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-09 17:50:54 -0700

    Create React App for light demo

---
+++ Week_a11/dark-mode-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BusinessCard from './BusinessCard';
+
+function App() {
+  return (
+    <>
+      <BusinessCard />
+    </>
+  );
+}
+
+export default App;
---
+++ Week_a11/dark-mode-demo/src/BusinessCard.js
@@ -0,0 +1,10 @@
+export default function BusinessCard() {
+    return(
+        <div>
+            <p>Cindy</p>
+            <p>Educator</p>
+            <p>cindyandcode@gmail.com</p>
+            <p>123-456-7890</p>
+        </div>  
+    )
+}
