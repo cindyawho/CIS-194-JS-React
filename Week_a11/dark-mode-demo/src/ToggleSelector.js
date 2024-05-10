7569a9fe6995e10c5b4924aadcb3ccc07a893471
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-09 20:05:57 -0700

    Create toggleSelector and styling

---
+++ Week_a11/dark-mode-demo/src/ToggleSelector.js
@@ -0,0 +1,16 @@
+export default function ToggleSelector() {
+    return (
+        <nav>
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div className="toggler--slider">
+                    <div className="toggler--slider--circle">
+                        
+                    </div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+                
+            </div>
+        </nav>
+    )
+}
