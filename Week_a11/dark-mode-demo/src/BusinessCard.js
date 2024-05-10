d47fea036265d5b8d29ff1dea29d8a1310fd4fea
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-09 19:41:37 -0700

    Update BusinessCard.js
    using props

--- Week_a11/dark-mode-demo/src/BusinessCard.js
+++ Week_a11/dark-mode-demo/src/BusinessCard.js
@@ -1,10 +1,26 @@
+function Name(props) {
+    return <h1>{props.name}</h1>
+}
+
+function Title(props) {
+    return <p>{props.title}</p>
+}
+
+function Email(props) {
+    return <p>{props.email}</p>
+}
+
+function Phone(props) {
+    return <p>{props.phone}</p>
+}
+
 export default function BusinessCard() {
     return(
         <div>
-            <p>Cindy</p>
-            <p>Educator</p>
-            <p>cindyandcode@gmail.com</p>
-            <p>123-456-7890</p>
+            <Name name = "Cindy"/>
+            <Title title = "Educator"/>
+            <Email email = "cindyandcode@gmail.com"/>
+            <Phone phone = "123-456-7890"/>
         </div>  
     )
 }
