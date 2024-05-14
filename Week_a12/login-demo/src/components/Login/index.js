7bee79f779e2bf491726fb6d82eb84ff3294f0fc
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-13 23:08:43 -0700

    Update react form and useState

--- Week_a12/login-demo/src/components/Login/index.js
+++ Week_a12/login-demo/src/components/Login/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+function fetchToken(creds) {
+
+}
 
 function Login( {setToken} ) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = (evt) => {
+        const token = fetchToken({
+            username, 
+            password,
+        });
+        setToken(token);
+    }
+
     return(
         <div className='login-wrapper'>
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit = {{handleSubmit}}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={evt => setUserName(evt.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={evt => setPassword(evt.target.value)}/>
                 </label>
                 <div>
                     <button type="submit">Submit</button>
