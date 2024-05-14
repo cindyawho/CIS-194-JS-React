c3d3abf43965da13df0745d9ceb650b95c907003
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-13 19:48:19 -0700

    visible Toggle Selector!

--- Week_a11/hw-9-business-card/src/App.js
+++ Week_a11/hw-9-business-card/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Info from './componentDirectory/Info';
 import About from './componentDirectory/About';
 import Funfacts from './componentDirectory/Funfacts';
-import Buttons from './componentDirectory/Buttons'
+import Buttons from './componentDirectory/Buttons';
+import ToggleSelector from './componentDirectory/ToggleSelector';
 
 function App() {
   return (
     <div className="Card">
+      <ToggleSelector/>
       <Info />
       <About />
       <Funfacts />
