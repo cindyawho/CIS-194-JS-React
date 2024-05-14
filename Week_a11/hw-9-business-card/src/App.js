72750779967f259b42c400821cc5bd49615d709c
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-13 19:52:24 -0700

    useState for toggleDarkMode

--- Week_a11/hw-9-business-card/src/App.js
+++ Week_a11/hw-9-business-card/src/App.js
@@ -4,11 +4,23 @@ import About from './componentDirectory/About';
 import Funfacts from './componentDirectory/Funfacts';
 import Buttons from './componentDirectory/Buttons';
 import ToggleSelector from './componentDirectory/ToggleSelector';
+import React, {useState} from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(mode => !mode);
+    // console.log("Hello");
+    // console.log(darkMode);
+  }
+
   return (
     <div className="Card">
-      <ToggleSelector/>
+      <ToggleSelector
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
       <Info />
       <About />
       <Funfacts />
