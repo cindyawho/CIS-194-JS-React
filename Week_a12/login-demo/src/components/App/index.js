11b3d2090b74a660eec1cefe0724b60bb62b5a3e
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-13 22:17:22 -0700

    Create routes and unique pages

--- Week_a12/login-demo/src/components/App/index.js
+++ Week_a12/login-demo/src/components/App/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Login from '../Login';
+import LoginPage from '../Login';
+import LandingPage from '../Landing';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
     return(
         <>
             <h1>App</h1>
-            <Login />
+            <BrowserRouter>
+            <Routes>
+                <Route path="/login" element ={<LoginPage />} />
+                <Route path="/home" element ={<LandingPage />} />
+            </Routes>
+            </BrowserRouter>
         </>
     );
 }
