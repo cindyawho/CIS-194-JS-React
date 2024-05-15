9dca8590d165f0588b202cd13ae7b2e17c027b2b
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-14 18:50:46 -0700

    Create components for my-notes
    Create body and header components

--- Week_a12/my-notes/src/App.js
+++ Week_a12/my-notes/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Header from './components/Header';
+import Body from './components/Body';
 function App() {
   return (
     <div className="App">
       My Project
-      Hello
+      <Header />
+      <Body />
     </div>
   );
 }
