e3ebc9c226a549911ba2fa41c64bcb7e7eb81760
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-14 19:10:26 -0700

    Use props to get note info

--- Week_a12/my-notes/src/components/ListItem.js
+++ Week_a12/my-notes/src/components/ListItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ListItem = () => {
-  return (
-    <div>
-        <h3>List Item</h3>
-    </div>
-  )
+const ListItem = (props) => {
+    console.log('PROPS: ', props);
+    return (
+        <div>
+            <h3>{props.note.body}</h3>
+        </div>
+    )
 }
 
 export default ListItem
