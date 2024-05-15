f6e84c8c5d8abf00282ab43edfec8c3708f8afd9
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-14 19:21:40 -0700

    Import React Router DOM
    npm install react-router-dom

--- Week_a12/my-notes/src/App.js
+++ Week_a12/my-notes/src/App.js
@@ -1,3 +1,8 @@
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
@@ -5,12 +10,14 @@ import NotePage from './pages/NotePage'
 
 function App() {
   return (
-    <div className="App">
-      My Project
-      <Header />
-      <NotesListPage />
-      <NotePage />
-    </div>
+    <Router>
+      <div className="App">
+        My Project
+        <Header />
+        <NotesListPage />
+        <NotePage />
+      </div>
+    </Router>
   );
 }
 
