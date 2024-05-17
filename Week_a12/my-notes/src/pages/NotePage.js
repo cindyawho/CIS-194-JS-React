ebf4d9cbf6d730f21e282c5eac2b25ff10450fa9
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-16 21:36:10 -0700

    Render unique note data per route
    - import notes data
    - find the note with a matching id in the data
    - render the note text with {note.body}

--- Week_a12/my-notes/src/pages/NotePage.js
+++ Week_a12/my-notes/src/pages/NotePage.js
@@ -2,13 +2,18 @@ import React from 'react'
 import {
   useParams
 } from "react-router-dom";
+import notesData from '../assets/data'
 
 const NotePage = (props) => {
   let noteId = useParams().userId;
-  console.log("noteId: ", noteId);
+  // console.log("noteId: ", noteId);
+
+  let note =  notesData.find(note => note.id === Number(noteId));
+
   return (
     <div>
         <h1>This is a single note page.</h1>
+        <p>{note.body}</p>
     </div>
   )
 }
