4bc751cc62fc98c82a47c250ed0106193e919609
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-05-27 15:00:12 -0700

    Implement Phone Update function
    uses evt.target.value
    useState
    and onChange

--- Week_a13/phone-number-validation/src/App.js
+++ Week_a13/phone-number-validation/src/App.js
@@ -1,16 +1,24 @@
+import React, {useState} from 'react'
 
 function App() {
+  const [number, setNumber] = React.useState("");
+
+  const update = evt => {
+    setNumber(evt.target.value);
+    return number
+  }
+
   return (
     <div>
       <header>
         <h1>Phone Number Validation</h1>
-        <h2>Phone Number Entered:</h2>
+        <h2>Phone Number Entered: {number}</h2>
       </header>
         <p>
           Enter Phone Number Below:
         </p>
         <form>
-          <input type="text"></input>
+          <input type="text" onChange={update}></input>
           <button>Validate</button>
         </form>
     </div>
